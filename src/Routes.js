7516c9726b298f1a91dcf3e92287e708209a874a
HenriqueMGA <riquemamaral@gmail.com>
2024-06-28 11:44:50 -0300

    Olá Mundo

---
+++ src/Routes.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "componentes/Menu";
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/Sobre Mim';
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
+import NaoEncontrado from "paginas/naoEncontrado";
+import ScrollToTop from "componentes/ScrollToTop";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
+
+        <Route path="posts/:id*" element={<Post />} />
+        <Route path="*" element={<NaoEncontrado />} />
+      </Routes>
+
+      <Rodape />
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
---
+++ src/paginas/Post/index.js
@@ -0,0 +1,53 @@
+import { useParams } from "react-router-dom"
+import posts from 'json/posts.json'
+import PostModelo from "componentes/PostModelo"
+import Markdown from "react-markdown"
+import './Post.css'
+import PaginaPadrao from "componentes/PaginaPadrao"
+import styles from './Post.module.css'
+import PostCard from "componentes/PostCard"
+
+export default function Post() {
+    const parametros = useParams()
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id)
+    })
+
+    if(!post) {
+        return <h1>Post não encontrado...</h1>
+    }
+
+    const postRecomendado = posts
+        .filter((post) => post.id !== Number(parametros.id))
+        .sort((a, b) => b.id - a.id)
+        .slice(0, 4)
+    
+    return (
+        <PaginaPadrao>
+            <PostModelo
+                fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                titulo={post.titulo}
+            >
+                <div className="post-markdown-container">
+                    <Markdown>
+                        {post.texto}
+                    </Markdown>
+                </div>
+
+                <h2 className={styles.tituloOutrosPosts}>
+                    Outros posts que você pode gostar:
+                </h2>
+
+                <ul className={styles.postsRecomendados}>
+                    {postsRecomendados.map((post) => {
+                        <li key={post.id}>
+                            <PostCard post={post} />
+                        </li>
+                    })}
+                </ul>
+            </PostModelo>
+        </PaginaPadrao>
+        
+    )
+}
